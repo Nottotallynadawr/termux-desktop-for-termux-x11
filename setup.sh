3afd72aca040a4d2a6d0d6ee7ac5b4bd6eff2008
Nottotallynadawr <140435821+Nottotallynadawr@users.noreply.github.com>
2023-11-27 15:21:04 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -260,20 +260,13 @@ termux-x11 -xstartup 'openbox-session'
 ## Start Graphical Server
 echo [!] Make Sure The X11 App Resolution Is Custom And Set To 1366x768! So It Fits Correctly And Everything Starts In 8 Secs
 sleep 8
-MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 virgl_test_server_android --angle-gl & > /dev/null 2>&1
+MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=4.3 virgl_test_server_android --angle-gl & > /dev/null 2>&1
 sleep 1
-## Start Termux-x11 Server And Open The App Somehow.
 XDG_RUNTIME_DIR=${TMPDIR} termux-x11 :1.0 &
 sleep 1
 am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity > /dev/null 2>&1
-## Start Openbox With Some Things
 sleep 1
 env DISPLAY=:1.0 GALLIUM_DRIVER=virpipe dbus-launch --exit-with-session openbox-session & > /dev/null 2>&1
-
-sleep 5
-process_id=$(ps -aux | grep '[x]fce4-screensaver' | awk '{print $2}')
-kill "$process_id" > /dev/null 2>&1
-
 	_EOF_
  
  chmod +x $file2
